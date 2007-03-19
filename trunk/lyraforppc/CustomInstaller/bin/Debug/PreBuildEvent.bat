985dc8e9653e23179288158c4808e02fa143a913
ogirard <ogirard@6ad169b7-271b-0410-b7fd-9618c16e8600>
2007-03-19 14:36:56 +0000

    branch to version 2.0

--- trunk/lyraforppc/CustomInstaller/bin/Debug/PreBuildEvent.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-..\..\..\PocketApp\BuildCabs\BuildCab.bat
-if errorlevel 1 goto CSharpReportError
-goto CSharpEnd
-:CSharpReportError
-echo Project error: A tool returned an error code from the build event
-exit 1
-:CSharpEnd
